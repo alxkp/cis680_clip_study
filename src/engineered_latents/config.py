5b4e5aadb679dcbc2446e8b43221faa03f4ea6b9
Alexander Kyimpopkin <39439486+alxkp@users.noreply.github.com>
2025-11-26 19:13:37 -0500

    created config structure

--- src/engineered_latents/config.py
+++ src/engineered_latents/config.py
@@ -1,3 +1,32 @@
 from dataclasses import dataclass
-from pathlib import Path
+from typing import Literal
 from draccus import ChoiceRegistry
+
+@dataclass 
+class ModelConfig(ChoiceRegistry):
+    pass
+
+@dataclass
+@ModelConfig.register_subclass('clip32')
+class ClipConfig(ModelConfig):
+    name:str = "openai/clip-vit-base-patch32"
+
+@dataclass
+class TrainConfig:
+    lr: float = 1e-4
+    optimizer: Literal['adam','adamw'] = "adamw"
+
+@dataclass
+class DatasetConfig(ChoiceRegistry):
+    name: str
+
+@dataclass
+@DatasetConfig.register_subclass("winoground")
+class WinogroundConfig(DatasetConfig):
+    name: str =  'facebook/winoground'
+
+@dataclass
+class MainConfig:
+    model: ModelConfig = ClipConfig()
+    dataset: DatasetConfig = WinogroundConfig()
+    train: TrainConfig = TrainConfig()
